62a1ddaf842758b5a23f3d34eccac1bde3d1cc0a
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-03-18 09:12:10 +0000

    chore: self mutation
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- src/flag-trigger/index.ts
+++ src/flag-trigger/index.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
 // https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.18.1/docs/resources/flag_trigger
 // generated from terraform resource schema
 
