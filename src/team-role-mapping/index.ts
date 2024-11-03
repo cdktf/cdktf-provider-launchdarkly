9d7dab0bbc75f2ef858c1e7773ab699da148457e
github-team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2024-11-03 06:03:44 +0000

    chore(deps): update  by cdktf/cdktf-repository-manager@c92583d

--- src/team-role-mapping/index.ts
+++ src/team-role-mapping/index.ts
@@ -1,8 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- */
-
 // https://registry.terraform.io/providers/launchdarkly/launchdarkly/2.21.0/docs/resources/team_role_mapping
 // generated from terraform resource schema
 
